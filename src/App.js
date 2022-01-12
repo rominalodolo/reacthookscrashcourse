fed22a0a395b5350c4b6340d6b29c2e312549fde
rominalodolo <lodoloromina@gmail.com>
2022-01-12 18:52:07 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import CharPicker from './components/CharPicker';
+import Character from './components/Character';
+
+class App extends Component {
+  state = {
+    selectedCharacter: 1,
+    side: 'light',
+    destroyed: false
+  };
+
+  sideHandler = side => {
+    this.setState({ side: side });
+  };
+
+  charSelectHandler = event => {
+    const charId = event.target.value;
+    this.setState({ selectedCharacter: charId });
+  };
+
+  destructionHandler = () => {
+    this.setState({ destroyed: true });
+  };
+
+  render() {
+    let content = (
+      <React.Fragment>
+        <CharPicker
+          side={this.state.side}
+          selectedChar={this.state.selectedCharacter}
+          onCharSelect={this.charSelectHandler}
+        />
+        <Character selectedChar={this.state.selectedCharacter} />
+        <button onClick={this.sideHandler.bind(this, 'light')}>
+          Light Side
+        </button>
+        <button onClick={this.sideHandler.bind(this, 'dark')}>Dark Side</button>
+        {this.state.side === 'dark' && (
+          <button onClick={this.destructionHandler}>DESTROY!</button>
+        )}
+      </React.Fragment>
+    );
+
+    if (this.state.destroyed) {
+      content = <h1>Total destruction!</h1>;
+    }
+    return content;
+  }
 }
 
 export default App;
